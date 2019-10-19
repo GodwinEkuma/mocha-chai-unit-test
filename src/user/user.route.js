29ba12216fea8ee4d16b929093c2974f6cddb100
Godwin Ekuma <godwin.ekuma@andela.com>
2019-10-19 16:56:05 +0100

    add a new route

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("./user.controller");
+const { userService } = require("./dependency");
 
-const userController = new UserController();
+const userController = new UserController(userService);
 
-router.post("/register", userController.register);
+router.post("/user", (req, res) => userController.register(req, res));
+router.get("/user/:id", (req, res) => userController.getUser(req, res));
 
 module.exports = router;
